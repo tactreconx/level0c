f1a15d5109670a4cab01dbe50dd6df1899d9ce97
tactreconx <league@Mac-mini-3.local>
2016-08-04 18:27:47 -0700

    Codecraker and a potatoe
    code crak and a potato

---
+++ Eclipse/src/Potatoe.java
@@ -0,0 +1,12 @@
+
+public class Potatoe {
+	public static void main(String[] args) {
+
+		for (int i = 1; i < 4; i = i + 1)
+			System.out.println(i + "Potatoes");
+		System.out.println("4");
+		for (int i = 5; i < 9; i = i + 1)
+			System.out.println(i + "Potatoes");
+		System.out.println("More");
+	}
+}
